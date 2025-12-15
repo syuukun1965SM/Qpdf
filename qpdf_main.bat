9d0dc7ab008575971f0874b5e6519d7def226b52
syuukun <syuuji.moriya.v30@mail.toshiba>
2025-12-15 14:42:23 +0900

    基本機能の実装
    このmainバッチは4つの選択肢に対してユーザ指定の機能を呼び出すスイッチャ(ランチャ)が基本機能。
    これが未実装だったので実装した。

--- qpdf_main.bat
+++ qpdf_main.bat
@@ -6,18 +6,53 @@ rem 各機能に紐づけした番号を表示し、入力数値により紐づけした機能別バッチファイル
 rem 分岐先処理が終了したら本バッチファイルにpromptを戻して上記機能選択待ちとする。
 
 
-echo 1 → 『パスワード設定』 を選択
-echo 2. オプション B を選択
-set /p choice=選択肢を入力してください: 
+:INPUT_LOOP
+rem 実施処理選択部
+echo.
+echo  ==実施する処理の選択==
+echo    1) 暗号化: ユーザによる指定pdfファイルを暗号化(パスワード付加)する。
+echo    2) 復号化: ユーザによる指定pdfファイルを復号化(パスワード解除)する。
+echo    3) ファイル回転: ユーザによる指定pdfファイル全体を指定した角度だけ回転する。
+echo    4) ページ抽出・削除・結合他: ユーザによる指定ファイル内のページに関連した各種処理を実行する。
+echo    5) 処理終了。
+echo.
 
-if "%choice%"=="1" (
-	echo pdfファイルにパスワードを設定します。
-	call qpdf_PW_apply.bat
-	REM ここにオプション A の処理を記述します。
-) else if "%choice%"=="2" (
-	echo オプション B が選択されました。
-	REM ここにオプション B の処理を記述します。
-) else (
-	echo 無効な選択肢です。
-	REM ここに無効な選択肢に対する処理を記述します。
+choice /C 12345 /M "処理を(1:暗号化、2:復号化、3:ファイル回転、4:ページ処理、５:終了)から選択してください"
+echo "%ERRORLEVEL%"
+PAUSE
+
+if ERRORLEVEL 5 goto :END
+
+if ERRORLEVEL 4 (
+	call %~dp0qpdf_pages.bat
 )
+if ERRORLEVEL 3 (
+	call %~dp0qpdf_rotate.bat
+)
+if ERRORLEVEL 2 (
+	call %~dp0qpdf_decrypt.bat
+)
+if ERRORLEVEL 1 (
+	call %~dp0qpdf_encrypt.bat
+)
+
+
+:CONTINUATION_CHECK
+rem 処理継続の意思確認部
+echo.
+choice /C YN /M "別の処理をしますか？ (Y/N)"
+echo.
+if ERRORLEVEL 2 goto :END
+if ERRORLEVEL 1 goto :INPUT_LOOP
+
+
+:END
+rem 終了処理部
+echo.
+echo +-------------------------------------------------------+
+echo  終了します。
+echo +-------------------------------------------------------+
+echo.
+
+exit /b
+
